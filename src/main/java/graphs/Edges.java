86671b8a3a16a6c1de988b4f0bc3e144a5c5bf0b
Achille Soulie <achille.soulie@isep.fr>
2019-04-17 11:15:56 +0200

    Ajout des classes Edges et Node

---
+++ src/main/java/graphs/Edges.java
@@ -0,0 +1,12 @@
+package graphs;
+
+public class Edges {
+    int destinationID;
+    int duration;
+    String type;
+    String hourOfOpening;
+    String hourOfClosing;
+    int lineNumber;
+    String lineDirection;
+    int frequency;
+}
---
+++ src/main/java/graphs/Node.java
@@ -0,0 +1,10 @@
+package graphs;
+
+public class Node {
+    int id;
+    String name;
+    double[] locations;
+    int zipcode;
+    Edges[] edges;
+
+}
