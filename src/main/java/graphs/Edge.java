9aec8d0d51054880409d71c24afc7df840a5bc12
guill <gjouetpastre@juniorisep.com>
2019-05-13 16:53:16 +0200

    Update Edges

--- src/main/java/graphs/Edge.java
+++ src/main/java/graphs/Edge.java
@@ -6,29 +6,34 @@ import static java.lang.Math.pow;
 
 public class Edge {
 
-    private Node originNode;
+    private Node sourceNode;
     private Node destinationNode;
+    /*distance entre les 2 lat/long des 2 nodes*/
+    private double weight;
+
+    /*Données supplémentaires*/
     private String type;
-    private double size;
     private String ligne;
 
-    public Edge(Node originNode, Node destinationNode, String type, String ligne){
-        this.originNode = originNode;
+    public Edge(Node sourceNode, Node destinationNode, String type, String ligne){
+        this.sourceNode = sourceNode;
         this.destinationNode = destinationNode;
+        this.weight = this.getDistance(sourceNode,destinationNode);
+
         this.type = type;
         this.ligne = ligne;
+    }
 
-        double x1 = Double.parseDouble(originNode.getLat());
-        double x2 = Double.parseDouble(destinationNode.getLat());
-        double y1 = Double.parseDouble(originNode.getLng());
-        double y2 = Double.parseDouble(destinationNode.getLng());
-
-        this.size = sqrt(pow((x1-x2),2)+pow((y1-y2),2)); //distance entre les 2 lat/long des 2 nodes
-
+    public double getDistance(Node origin, Node destination){
+        double x1 = Double.parseDouble(origin.getLat());
+        double x2 = Double.parseDouble(destination.getLat());
+        double y1 = Double.parseDouble(origin.getLng());
+        double y2 = Double.parseDouble(destination.getLng());
+        return sqrt(pow((x1-x2),2)+pow((y1-y2),2));
     }
 
     public Node getOriginNode() {
-        return originNode;
+        return sourceNode;
     }
 
     public Node getDestinationNode() {
@@ -43,11 +48,11 @@ public class Edge {
         return ligne;
     }
 
-    public double getSize() {
-        return size;
+    public double getWeight() {
+        return weight;
     }
 
     public String toString(){
-        return "From: "+getOriginNode().getNom()+" to: "+getDestinationNode().getNom()+" via: "+getType()+" "+getLigne()+" distance : "+getSize() +"\n";
+        return "From: "+getOriginNode().getNom()+" to: "+getDestinationNode().getNom()+" via: "+getType()+" "+getLigne()+" distance : "+getWeight() +"\n";
     }
 }
